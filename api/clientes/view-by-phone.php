da40c7201aa410d2d464ad809120189251f59477
Alison <alisonsarto07@gmail.com>
2025-02-02 20:51:28 -0300

    Gambiarra

---
+++ api/clientes/view-by-phone.php
@@ -0,0 +1,48 @@
+<?php
+
+  include $_SERVER['DOCUMENT_ROOT'] . '/funcs/config.php';
+
+  $phone = $_GET['phone'] ?? null;
+
+  if ($phone == null) {
+    send([
+      'status' => 400,
+      'message' => 'Telefone não informado'
+    ]);
+  }
+
+
+  $phone = str_replace('+', '', $phone);
+  if (substr($phone, 0, 2) == '55') {
+    $phone = substr($phone, 2);
+  }
+
+
+  $url = "clientes";
+  $method = 'GET';
+  $data = [];
+  $data = [
+    'telefone' => $phone
+  ];
+
+  $response = gs_click($url, $method, $data);
+
+  if ($response == "Nenhum cliente foi encontrado!") {
+    send([
+      'status' => 400,
+      'message' => 'Cliente não encontrado'
+    ]);
+  }
+
+  $cliente_id = $response[0]['id'];
+  $cliente_id = base64_encode($cliente_id);
+
+  $magic = "https://www.alohaaapp.com.br/api/login/magic?access=$cliente_id";
+
+  send([
+    'status' => 200,
+    'link' => $magic,
+    'phone' => $phone
+  ]);
+
+?>
