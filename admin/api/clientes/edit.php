5ff93adabc27408321cdb75972e20a9e707467da
Ali <119765430+AlisonSarto@users.noreply.github.com>
2025-04-17 18:03:14 -0300

    Update edit.php

--- admin/api/clientes/edit.php
+++ admin/api/clientes/edit.php
@@ -10,11 +10,10 @@
   }
 
   $cliente_id = $_POST['id'] ?? null;
-  $email = $_POST['email'] ?? null;
   $vlr_frete = $_POST['vlr_frete'] ?? null;
   $vlr_pacote = $_POST['vlr_pacote'] ?? null;
 
-  if (!$cliente_id || !$email || !$vlr_frete || !$vlr_pacote) {
+  if (!$cliente_id || !$vlr_frete || !$vlr_pacote) {
     send([
       'status' => 400,
       'message' => 'Parâmetros inválidos'
@@ -32,19 +31,8 @@
     ]);
   }
 
-  //? Verifica se o email já está em uso
-  $sql = "SELECT * FROM usuarios WHERE email = '$email' AND cliente_id != $cliente_id";
-  $res = $conn->query($sql);
-
-  if ($res->num_rows > 0) {
-    send([
-      'status' => 400,
-      'message' => 'Email já cadastrado'
-    ]);
-  }
-
   //? Atualiza o cliente
-  $sql = "UPDATE usuarios SET email = '$email', vlr_frete = $vlr_frete, vlr_pacote = $vlr_pacote WHERE cliente_id = $cliente_id";
+  $sql = "UPDATE usuarios SET vlr_frete = $vlr_frete, vlr_pacote = $vlr_pacote WHERE cliente_id = $cliente_id";
   $conn->query($sql);
 
   send([
